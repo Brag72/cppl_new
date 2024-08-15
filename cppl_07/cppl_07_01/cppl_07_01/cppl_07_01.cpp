35cecb4b986067d3db2462f50e443d44a1ef96b7
Brag72 <49995005+Brag72@users.noreply.github.com>
2024-08-15 22:32:45 +0300

    lesson 8

---
+++ cppl_07/cppl_07_01/cppl_07_01/cppl_07_01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+	std::vector<int> Vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	std::cout << "[IN]: ";
+
+	for (int i : Vec)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+
+	std::sort(Vec.begin(), Vec.end());
+	
+	auto it = std::unique(Vec.begin(), Vec.end());
+
+	std::cout << "[OUT]: ";
+	Vec.erase(it, Vec.end());
+	for (int i : Vec)
+	{
+		std::cout << i << " ";
+	}
+}
---
+++ cppl_07/cppl_07_02/cppl_07_02/cppl_07_02.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include<set>
+#include<list>
+
+template<typename T>
+void print_container(T cont, size_t size)
+{
+	for (auto i : cont)
+	{
+		if (size == 1)
+		{
+			std::cout << i;
+			break;
+		}
+		std::cout << i << ", ";
+		size--;
+	}
+}
+
+
+int main()
+{
+	std::vector<std::string> test_vector{ "one", "two", "three", "four" };
+	print_container(test_vector, test_vector.size());
+	std::cout << std::endl;
+
+	std::set<std::string> test_set{ "one", "two", "three", "four" };
+	print_container(test_set, test_set.size());
+	std::cout << std::endl;
+
+	std::list<std::string> test_list{ "one", "two", "three", "four" };
+	print_container(test_list, test_list.size());
+}
